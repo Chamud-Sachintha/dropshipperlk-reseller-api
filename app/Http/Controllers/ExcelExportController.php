c2fd1b617b9c8d6c0daa202f7525d33162d4f0c2
Chamud-Sachintha <sheraanmario777@gmail.com>
2024-08-20 17:05:02 +0530

    bug fix

--- app/Http/Controllers/ExcelExportController.php
+++ app/Http/Controllers/ExcelExportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exports\SimpleExcelExport;
 use App\Helpers\AppHelper;
+use App\Models\Reseller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
@@ -12,10 +13,12 @@ class ExcelExportController extends Controller
 {
 
     private $AppHelper;
+    private $Reseller;
 
     public function __construct()
     {
         $this->AppHelper = new AppHelper();
+        $this->Reseller = new Reseller();
     }
 
     public function DownloadExcel(Request $request)
@@ -23,11 +26,7 @@ class ExcelExportController extends Controller
         // You can add validation or authorization logic here if needed
         set_time_limit(300); // 300 seconds = 5 minutes
 
-        $sellerId = (is_null($request->sellerId) || empty($request->sellerId)) ? "" : $request->sellerId;
-
-        if ($sellerId == "") {
-            return $this->AppHelper->responseMessageHandle(0, "Invalid Seller ID");
-        }
+        $seller_info = $this->Reseller->find_by_token($request->token);
         
         $selectedReportType = $request->input('selectedReportType');
         $token = $request->input('token');
@@ -41,7 +40,7 @@ class ExcelExportController extends Controller
             return $this->AppHelper->responseMessageHandle(0, "Invalid Type");
         }
 
-        $export = new SimpleExcelExport($selectedReportType, $sellerId);
+        $export = new SimpleExcelExport($selectedReportType, $seller_info->id);
 
         $filename = $typerepo . '_' . Carbon::now()->format('Y-m-d_H-i-s');
 
